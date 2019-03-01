0fb8585995f0daa78e19208e39ee4b58c4122d79
Caio Alcântara <caiocomputers@gmail.com>
2019-02-28 23:32:16 -0300

    Initial setup, cocoapods libs that will be used

---
+++ Podfile
@@ -0,0 +1,13 @@
+platform :ios, '11.0'
+
+target 'picpayment' do
+  
+  use_frameworks!
+
+  pod "KRProgressHUD"
+  pod 'IQKeyboardManagerSwift'
+  pod 'SDWebImage', '~> 4.0'
+  pod 'ReachabilitySwift'
+  pod 'Alamofire', '~> 4.7'
+
+end
---
+++ picpayment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  picpayment
+//
+//  Created by Caio Alcântara on 28/02/19.
+//  Copyright © 2019 Red Ice. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
